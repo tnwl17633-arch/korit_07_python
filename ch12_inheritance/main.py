466aefd82be607c486993eb2c8f3c1a6cb2f8ac3
tnwl17633 <tnwl9974@gmail.com>
2025-09-12 17:47:16 +0900

    feat: 주말 잘 보내세요

--- ch12_inheritance/main.py
+++ ch12_inheritance/main.py
@@ -8,21 +8,21 @@ class 슈퍼클래스 :
 class 서브클래스(슈퍼클래스):
     본문
 '''
-# class Person:
-#     def __init__(self, name):
-#         self.name = name
-#
-#     def eat(self, food):
-#         print(f'{self.name}이(가) {food}를 먹습니다.')
-#
-# class Student(Person):
-#
-#     def __init__(self, name, school):
-#         super().__init__(name)
-#         self.school = school
-#
-#     def study(self):
-#         print(f'{self.name}은(는) {self.school}에서 공부를 합니다.')
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def eat(self, food):
+        print(f'{self.name}이(가) {food}를 먹습니다.')
+
+class Student(Person):
+
+    def __init__(self, name, school):
+        super().__init__(name)
+        self.school = school
+
+    def study(self):
+        print(f'{self.name}은(는) {self.school}에서 공부를 합니다.')
 #
 #
 # # 객체 생성
